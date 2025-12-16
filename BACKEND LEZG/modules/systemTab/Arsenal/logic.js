76b27b361579317f7e897618483da2a34b44db54
onlinebizniluis-lgtm <onlinebizniluis@gmail.com>
2025-12-17 02:38:06 +0800

    Delete BACKEND LEZG directory

--- BACKEND LEZG/modules/systemTab/Arsenal/logic.js
+++
@@ -1,8 +0,0 @@
-window.ENERGY_COOLDOWN = 4 * 60 * 60 * 1000; // 4 hours
-window.FLUX_COOLDOWN   = 6 * 60 * 60 * 1000; // 6 hours
-
-window.forgeEnergy = 3;
-window.fluxEnergy  = 8;
-
-window.MAX_ENERGY = 3;
-window.MAX_FLUX   = 8;
